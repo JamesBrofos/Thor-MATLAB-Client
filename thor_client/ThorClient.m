aa99515f0fdf913aff8b2f615c77235b8f267784
JamesBrofos <jamesbrofos@gmail.com>
2017-04-27 07:08:15 -0400

    start adding doc

--- thor_client/ThorClient.m
+++ thor_client/ThorClient.m
@@ -1,4 +1,28 @@
 classdef ThorClient
+    % Thor Client Class
+    %
+    % The purpose of this module is to allow MATLAB users to utilize the Thor
+    % utility for Bayesian optimization of machine learning systems. This module
+    % in particular is intended to properly authenticate with Thor and to create
+    % experiments.
+    %
+    % Parameters:
+    %     auth_token (str): String containing a user's specific API key provided
+    %         by the Thor server. This is used to authenticate with the Thor
+    %         server as a handshake that these experiments belong to a user and
+    %         can be viewed and edited by them.
+    %
+    % Examples:
+    %     The Thor Client can be used to create experiments as follows:
+    %
+    %     >> tc = ThorClient('YOUR_API_KEY');
+    %     >> dims = struct('name', 'x', 'dim_type', 'linear', 'low', 0., 'high', 1.);
+    %     >> exp = tc.create_experiment('YOUR_EXPERIMENT_NAME', dims);
+    %
+    %     Alternatively, the Thor Client can be used to query for existing
+    %     experiments.
+    %
+    %     >> exp = tc.experiment_for_name('YOUR_EXISTING_EXPERIMENT');
     properties
         auth_token
     end
@@ -7,8 +31,14 @@ classdef ThorClient
             % Initialize the parameters of the Thor API client.
             obj.auth_token = auth_token;
         end
-        function exp = create_experiment(obj, name, dimensions, acq_func)
+        function exp = create_experiment(obj, name, dimensions, acq_func,    ...
+                                         overwrite)
             % Create an experiment.
+            if nargin < 5
+                % By default do not overwrite experiments if they exist on the
+                % Thor server.
+                overwrite = false;
+            end
             if nargin < 4
                 % Make the specification of the acquisition function an
                 % optional parameter that defaults to the hedging strategy.
@@ -21,6 +51,7 @@ classdef ThorClient
             data = struct('name', name,                                      ...
                 'auth_token', obj.auth_token,                                ...
                 'acq_func', acq_func,                                        ...
+                'overwrite', overwrite,                                      ...
                 'dimensions', dimensions);
             res = webwrite(url, data, options);
             exp = ExperimentClient(res.id, res.name, res.date, ...
--- thor_client/base_url.m
+++ thor_client/base_url.m
@@ -1,4 +1,24 @@
 function ret = base_url(api_endpoint)
+% Constructs the URL for an API endpoint for the Thor Server application.
+%
+% The url that should be used in contact the Thor server. When Thor is deployed
+% locally, it make sense to use the localhost designation. On the other hand,
+% when deployed remotely, a particular URL needs to be provided.
+%
+% Parameters:
+%     api_endpoint (str): The endpoint to query. This is a string that makes
+%         sense for the enpoint (e.g. `create_experiment` for creating an
+%         experiment using Thor).
+%
+% Examples:
+%     To send data to the API endpoint to create an experiment, execute:
+%
+%     >> url = base_url('create_experiment');
+%
+%     To request a recommendation, the URL is,
+%
+%     >> url = base_url('create_recommendation');
+
 % ret = sprintf('http://192.168.1.169:5000/api/%s/', api_endpoint);
 ret = sprintf('http://aa-lnx01.mitre.org:5000/api/%s/', api_endpoint);
 end
